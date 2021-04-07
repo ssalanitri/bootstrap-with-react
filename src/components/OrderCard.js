7ecc449b6e325eaef2fdbbaee65a6ab997030c31
Sergio Gustavo Salanitri Montenegro <l0697469@bancogalicia.com.ar>
2021-04-07 18:54:34 -0300

    examples added

--- src/components/OrderCard.js
+++ src/components/OrderCard.js
@@ -1,30 +1,33 @@
 import React from 'react'
-import {  Card, Badge, Button } from 'react-bootstrap';
+import {  Card, Badge, Button, Row, Col } from 'react-bootstrap';
+
 
 
 const OrderCard = ({data, setOrdered}) => {
     return (
         <>
+        <Card className="m-4 border-0 shadow container position-relative">
+            <Row className="card-fluid">
             <Card className="h-100 shadow-sm bg-white rounded">
-                <Card.Img  className="images" variant="top" src={data.image} />
-                <Card.Body className="d-flex flex-column" >
-                    <div className = "d-flex mb-2 justify-content-between">
-                        <Card.Title className="mb-0 font-weight-bold">{data.name}</Card.Title>
-                        <Badge pill className="mb-2" variant="warning" >
-                            ${data.price}
-                        </Badge>
-                    </div>    
-                    <Card.Text className="text-secondary">{data.desc}</Card.Text>
-                    <Button
-                        onClick={() => setOrdered()}
-                        className="mt-auto font-weigth-bold"
-                        variant="success"
-                        block
-                    >
-                        Order Pizza 
-                    </Button>
-                </Card.Body> 
-            </Card>
+                    <Card.Img  className="images img-fluid" variant="top" src= {data.image} />
+                    <Card.Body className="d-flex flex-column" >
+                    <Card.Title className="mb-0 font-weight-bold">{data.name}</Card.Title>
+                    <Badge pill className="mb-2" variant="warning" >
+                        ${data.price}
+                    </Badge>
+                    <Card.Text className="card-text text-desc">{data.desc}</Card.Text>
+                        <Button
+                            onClick={() => setOrdered()}
+                            className="mt-auto font-weigth-bold"
+                            variant="success"
+                            block
+                        >
+                            Order Pizza 
+                        </Button>
+                    </Card.Body> 
+                </Card>
+            </Row>
+        </Card>
         </>
     )
 }
